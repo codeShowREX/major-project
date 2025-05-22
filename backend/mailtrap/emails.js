c9e54bda167a9528b3633f33b96401ea79163237
Shubhankar Majumdar <shubhankarmajumdar38@gmail.com>
2025-05-23 04:35:59 +0530

    Initial commit

--- backend/mailtrap/emails.js
+++ backend/mailtrap/emails.js
@@ -33,7 +33,7 @@ export const sendWelcomeEmail = async (email, name) => {
 		const response = await mailtrapClient.send({
 			from: sender,
 			to: recipient,
-			template_uuid: "6e7a24a9-2113-41f4-9096-542b51ae8670",
+			template_uuid: "cee78d39-ab4d-4fa4-a44c-9d8ac8d4bbd8",
 			template_variables: {
 				company_info_name: "Mern-Auth-Services",
 				name: name,
